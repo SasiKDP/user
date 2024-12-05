d43d74ce585e3eb6b7631c80d667e7befa8e00b2
VadaparthiVinuthna <vadaparthivinuthna@gmail.com>
2024-12-05 14:06:30 +0530

    committing all changes

--- src/main/java/com/dataquadinc/DataquadUserRegisterApiApplication.java
+++ src/main/java/com/dataquadinc/DataquadUserRegisterApiApplication.java
@@ -2,8 +2,10 @@ package com.dataquadinc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DataquadUserRegisterApiApplication {
 
 	public static void main(String[] args) {
