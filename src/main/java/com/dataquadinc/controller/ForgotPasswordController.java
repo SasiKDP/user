f037a37cb40d6fbfb51b9f2d7dfd15a906e139af
vadaparthivinuthna <vadaparthivinuthna@gmail.com>
2025-01-24 15:08:32 +0530

    added gcp ip

--- src/main/java/com/dataquadinc/controller/ForgotPasswordController.java
+++ src/main/java/com/dataquadinc/controller/ForgotPasswordController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://192.168.0.140:3000")
+@CrossOrigin(origins = "http://35.188.150.92")
 // Allow all origins
 
 @RestController
