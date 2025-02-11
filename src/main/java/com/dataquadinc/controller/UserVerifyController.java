a82ecb8bd8d73a0b09d53810088b3a123e7463b3
vadaparthivinuthna <vadaparthivinuthna@gmail.com>
2025-02-11 10:58:16 +0530

    added userverifyotp module

---
+++ src/main/java/com/dataquadinc/controller/UserVerifyController.java
@@ -0,0 +1,35 @@
+package com.dataquadinc.controller;
+
+import com.dataquadinc.dto.UserVerifyDto;
+import com.dataquadinc.service.UserVerifyService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserVerifyController {
+
+    private final UserVerifyService userService;
+
+    public UserVerifyController(UserVerifyService userService) {
+        this.userService = userService;
+    }
+
+    // Send OTP to Email
+    @PostMapping("/send-otp")
+    public ResponseEntity<String> sendOtp(@RequestParam String email) {
+        String response = userService.sendOtp(email);
+        return ResponseEntity.ok(response);
+    }
+
+    // Verify OTP
+    @PostMapping("/verify-otp")
+    public ResponseEntity<String> verifyOtp(@RequestBody UserVerifyDto userDTO) {
+        if (userDTO.getEmail() == null || userDTO.getOtp() == null) {
+            return ResponseEntity.badRequest().body("Email and OTP are required");
+        }
+        String response = userService.verifyOtp(userDTO);
+        return ResponseEntity.ok(response);
+    }
+}
+
--- src/main/java/com/dataquadinc/exceptions/UserAlreadyLoggedInException.java
+++ src/main/java/com/dataquadinc/exceptions/UserAlreadyLoggedInException.java
@@ -1,6 +1,10 @@
 package com.dataquadinc.exceptions;
 
+<<<<<<< Updated upstream
 public class UserAlreadyLoggedInException extends RuntimeException {
+=======
+public class UserAlreadyLoggedInException extends RuntimeException{
+>>>>>>> Stashed changes
     public UserAlreadyLoggedInException(String message) {
         super(message);
     }
