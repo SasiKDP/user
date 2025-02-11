8f66a0352a3dee8aa7f538ebbd5cc8cfceccaf1a
vadaparthivinuthna <vadaparthivinuthna@gmail.com>
2025-02-11 11:10:35 +0530

    modified userlogged module

--- src/main/java/com/dataquadinc/exceptions/UserAlreadyLoggedInException.java
+++ src/main/java/com/dataquadinc/exceptions/UserAlreadyLoggedInException.java
@@ -1,10 +1,8 @@
 package com.dataquadinc.exceptions;
 
-<<<<<<< Updated upstream
-public class UserAlreadyLoggedInException extends RuntimeException {
-=======
+
 public class UserAlreadyLoggedInException extends RuntimeException{
->>>>>>> Stashed changes
+
     public UserAlreadyLoggedInException(String message) {
         super(message);
     }
