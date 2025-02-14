8e98bb36fb5afb46776803b4cbf6b1d1e4a8f5df
vadaparthivinuthna <vadaparthivinuthna@gmail.com>
2025-02-14 10:59:01 +0530

    committing the leave calendar

--- src/main/java/com/dataquadinc/service/EmailService.java
+++ src/main/java/com/dataquadinc/service/EmailService.java
@@ -5,6 +5,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 public class EmailService {
 
@@ -12,12 +14,17 @@ public class EmailService {
     private JavaMailSender javaMailSender;
 
 
-    public void sendEmail(String to, String subject, String text)  {
+    public void sendEmails(Set<String> recipients, String subject, String text) {
+        String[] recipientArray = recipients.toArray(new String[0]);
+        sendEmail(recipientArray, subject, text);
+    }
+
+    public void sendEmail(String[] to, String subject, String text)  {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to); // recipient's email
         message.setSubject(subject); // email subject
         message.setText(text); // email body
-        message.setFrom("datamatrrrix@gmail.com"); // your email
+        mailto:message.setFrom("datamatrrrix@gmail.com"); // your email
 
         javaMailSender.send(message);
     }
