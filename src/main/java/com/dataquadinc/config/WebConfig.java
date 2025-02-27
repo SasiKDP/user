9bfb5a072572323fdf8d30046c887c598a2d73e1
vadaparthivinuthna <vadaparthivinuthna@gmail.com>
2025-02-27 14:56:24 +0530

    committing the http

--- src/main/java/com/dataquadinc/config/WebConfig.java
+++ src/main/java/com/dataquadinc/config/WebConfig.java
@@ -17,13 +17,13 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://192.168.0.140:3000",  // Second IP
                         "http://192.168.0.139:3000", // Third IP
                         "https://mymulya.com", // Forth IP
-                        "https://localhost:3000" // Fifth IP
+                        "http://localhost:3000" // Fifth IP
                 )
 
                 .allowedOrigins("http://35.188.150.92",  "http://192.168.0.140:3000",  // Second IP
                         "http://192.168.0.139:3000", // Third IP
                         "https://mymulya.com", // Forth IP
-                        "https://localhost:3000") // Fifth IP
+                        "http://localhost:3000") // Fifth IP
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
                 .allowedHeaders("*")                      // Allow all headers
                 .allowCredentials(true);                  // Allow credentials (cookies, headers, etc.)
