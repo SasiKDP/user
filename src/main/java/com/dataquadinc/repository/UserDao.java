e98c3373a9e733c10f0955754732cf4b495b1a09
Charani <charani@Charanis-MacBook-Air.local>
2025-02-28 10:44:54 +0530

    added personalemail validation

--- src/main/java/com/dataquadinc/repository/UserDao.java
+++ src/main/java/com/dataquadinc/repository/UserDao.java
@@ -2,6 +2,8 @@ package com.dataquadinc.repository;
 
 import com.dataquadinc.model.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import  java.util.*;
 
@@ -13,4 +15,7 @@ public interface UserDao extends JpaRepository<UserDetails,Integer> {
     UserDetails findByUserName(String userName);
 
     List<UserDetails> findByRolesId(Long id);
+    @Query("SELECT u FROM UserDetails u WHERE u.personalemail = :personalemail")
+    UserDetails findByPersonalEmail(String personalemail);
+    ;
 }
